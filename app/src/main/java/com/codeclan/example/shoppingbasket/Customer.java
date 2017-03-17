7260d767aa1dae1618138aedb9f2fa56a485c792
John Daly <jpdaly@gmail.com>
2017-03-17 21:19:42 +0000

    add customer class to track loyalty card

---
+++ app/src/main/java/com/codeclan/example/shoppingbasket/Customer.java
@@ -0,0 +1,14 @@
+package com.codeclan.example.shoppingbasket;
+
+/**
+ * Created by user on 17/03/2017.
+ */
+
+public class Customer {
+
+    private boolean hasLoyaltyCard;
+
+    public Customer(Boolean hasLoyaltyCard){
+        this.hasLoyaltyCard = hasLoyaltyCard;
+    }
+}
